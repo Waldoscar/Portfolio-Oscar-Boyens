97bbd57c4159a7cd7054d938359fa3ec05def7fe
Waldoscar <oscarboyens@gmail.com>
2024-06-18 11:04:49 +0200

    impressum deploy test: path: zu vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import path, { resolve } from 'path';
 export default defineConfig({
     build: {
         outDir: 'dist',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                key: resolve(__dirname, 'klien.html','impressum.html'),
+                key: resolve(__dirname, 'klien.html'),
+                path: resolve(__dirname,'impressum.html'),
             
             }
         }
