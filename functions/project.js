f9f1341ea628f330c8afaa1820048aa949409b95
Waldoscar <oscarboyens@gmail.com>
2025-02-24 10:42:52 +0100

    Logo in Nav bar
    Bento Pop-up close verbessert
    und auf mobile disabled

--- functions/project.js
+++ functions/project.js
@@ -1,7 +1,15 @@
 // Get the modal
 // export var modal = document.getElementById("myModal");
 export const modal = document.querySelector('.modal')
-
+// Get the <span> element that closes the modal
+export var span = document.getElementsByClassName("close")[0];
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  
+  if (!isMobile()) {
+  //place script you don't want to run on mobile here
+  
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 document.querySelectorAll('.bento-img').forEach(img => {
   img.onclick = () =>{
@@ -10,10 +18,9 @@ document.querySelectorAll('.bento-img').forEach(img => {
     document.getElementById("caption").src = img.getAttribute('alt');
   }
 });
-// Get the <span> element that closes the modal
-export var span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
-}
+}}
